c68cdc55ef68a4537d6f450a2a44fc19b4c9ab9e
rsdingle <rsdingle@wisc.edu>
2024-05-03 10:11:22 -0500

    some of the comment changes

--- Muthu/final/CPU/ALU.sv
+++ Muthu/final/CPU/ALU.sv
@@ -1,18 +1,20 @@
 `default_nettype none
 
 module ALU(
-    input wire logic [31:0] A, 
-    input wire logic[31:0] B,
-    input wire logic[3:0] ALU_op,
-    output logic [31:0] out
-);
+    ///// INPUTS  /////
+    input logic [31:0] A,      
+    input logic[31:0] B,
+    input logic[3:0] ALU_op,   
 
+    ///// OUTPUTS  /////
+    output logic [31:0] out  
+);
 
 always_comb begin : ALU_block
     case(ALU_op)
-        4'd0: out = A + B;
-        4'd1: out = A - B;
-        4'd2: out = A << B[4:0];
+        4'd0: out = A + B; //ADD
+        4'd1: out = A - B; //SUB
+        4'd2: out = A << B[4:0]; // SLL
         4'd3: begin // SLT
             if($signed(A) < $signed(B)) begin
                 out = 32'b1;
@@ -29,15 +31,14 @@ always_comb begin : ALU_block
                 out = 32'b0;
             end
         end
-        4'd5: out = A ^ B;
+        4'd5: out = A ^ B; // XOR
         4'd6: out = A >> B[4:0]; // SRL
         4'd7: out = $signed(A) >>> B[4:0]; // SRA
-        4'd8: out = A | B;
-        4'd9: out = A & B;
+        4'd8: out = A | B; // OR
+        4'd9: out = A & B; // AND
         default: out = 32'b0;
     endcase
 end
-
 
 endmodule
 
